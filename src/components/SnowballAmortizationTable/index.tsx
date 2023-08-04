f8ecabacbd5621b6a563f1b99d8d67756dd42343
Chase Skibeness <chaseskibbs@gmail.com>
2023-08-03 23:00:09 -0700

    add more to table

--- src/components/SnowballAmortizationTable/index.tsx
+++ src/components/SnowballAmortizationTable/index.tsx
@@ -20,23 +20,42 @@ class AccountSnapshot extends Account {
     this.totalDebt = totalDebt;
     this.extraPayment = extraPayment;
     this.snowballBonus = snowballBonus;
+    liabilities.forEach((liability) => {
+      this[liability.name] = liability.principal;
+    });
   }
 }
 
 export function SnowballAmortizationTable({
   account,
 }: SnowballAmortizationTableProps) {
-  const firstSnapshot = new AccountSnapshot(new Date(), account);
-  firstSnapshot.liabilities.forEach((liability) => {
-    firstSnapshot[liability.name] = liability.principal;
+  const snapshots = [new AccountSnapshot(new Date(), account)];
+  const accountKeys = Object.keys(snapshots[0]).filter((key) => {
+    switch (key) {
+      case "date":
+        return false;
+      case "totalDebt":
+        return false;
+      case "liabilities":
+        return false;
+      case "extraPayment":
+        return false;
+      case "snowballBonus":
+        return false;
+      default:
+        return true;
+    }
   });
-  console.log(firstSnapshot);
+
   return (
-    <DataTable value={account.liabilities}>
-      <Column field="name" header="Account Name" />
-      <Column field="principal" header="Total Owed" />
-      <Column field="minPayment" header="Payment Per Month" />
-      <Column field="aprStr" header="Interest Rate" />
+    <DataTable value={snapshots}>
+      <Column field="date" header="Month / Year" />
+      {accountKeys.map((accountKey) => (
+        <Column field={accountKey} header={accountKey} key={accountKey} />
+      ))}
+      <Column field="totalDebt" header="Total Owed" />
+      <Column field="extraPayment" header="Extra Payment Per Month" />
+      <Column field="snowballBonus" header="Snowball Bonus" />
     </DataTable>
   );
 }
